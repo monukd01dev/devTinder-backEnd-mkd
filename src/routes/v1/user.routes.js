25935bdc01019b415c8a9a7eec04eba3e785d911
MonuKD-2026 <monukd01dev@gmail.com>
2026-08-03 20:27:49 +0530

    caseSensitive import error on server resolve

--- src/routes/v1/user.routes.js
+++ src/routes/v1/user.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const UserController = require('../../controllers/user.Controller');
+const UserController = require('../../controllers/user.controller');
 const userRouter = express.Router();
 
 userRouter.get('/feed', UserController.userFeed)
